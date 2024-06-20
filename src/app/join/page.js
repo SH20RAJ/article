944735a45de81d2709601695d12c39f1fa843ba4
sh20raj <shaswatraj3@gmail.com>
2024-06-20 22:14:38 +0530

    NextAuth Init

--- src/app/join/page.js
+++ src/app/join/page.js
@@ -1,6 +1,7 @@
+"use client"
 import { signIn } from "next-auth/react"
 import Posts from "@/auth"
  
-export function SignIn() {
+export default function SignIn() {
   return <button onClick={() => signIn()}>Sign In</button>
 }
